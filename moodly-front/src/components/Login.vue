7659eefe6df8dd13ad547a0ee2680e7bed4d2dea
capucineleroy <91596703+capucineleroy@users.noreply.github.com>
2023-12-19 17:02:40 +0100

    create folder views

---
+++ moodly-front/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 class="title"> Moodly </h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.title {
+  color : white;
+  text-align: center;
+
+}
+</style>
